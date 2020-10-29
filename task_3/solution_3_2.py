65e72a11d743d1ca64d55aedd1a6fc1ed9e56939
polinadumbledore <72070019+polinadumbledore@users.noreply.github.com>
2020-10-29 20:34:52 +0300

    обновила решение 3.2

--- task_3/solution_3_2.py
+++ task_3/solution_3_2.py
@@ -1,6 +1,11 @@
-# your code here
-for smth in 'a'*10:
-    # your code here
-    user_input = input()
-# your code here
-print(alphabet)
+t = ""
+for k in range(10):
+    s = input()
+    for i in s:
+        in_t = False
+        for j in t:
+            if i == j:
+                in_t = True
+        if in_t == False:
+            t += i
+print(t)
