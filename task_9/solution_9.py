7820291ab66ba85f759b8682d54ae0289d33cdb8
polinadumbledore <72070019+polinadumbledore@users.noreply.github.com>
2020-10-29 22:38:25 +0300

    обновила решение 9

--- task_9/solution_9.py
+++ task_9/solution_9.py
@@ -1,7 +1,7 @@
 n = 0
 for i in range(1,101):
-	n += 2*i - 1
-if i*i == n:
-	print('True')
-else:
-  print('False')
+    n += 2*i - 1
+    if i*i == n:
+        print('True')
+    else:
+        print('False')
