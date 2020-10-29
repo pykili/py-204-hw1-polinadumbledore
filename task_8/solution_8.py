64f8d45b786eeced762d36012c07ced582f464f0
polinadumbledore <72070019+polinadumbledore@users.noreply.github.com>
2020-10-29 18:47:47 +0300

    поменяла названия переменных

--- task_8/solution_8.py
+++ task_8/solution_8.py
@@ -1,15 +1,15 @@
-a = 0
+summ = 0
 n = int(input())
 b = int(input())
-summ = 0
+amount = 0
 if b == 0:
     print(0) 
 else:
     while b != 0:
-        a += b
-        summ += 1
+        summ += b
+        amount += 1
         b = int(input())
-    print(a/summ)
+    print(summ/amount)
 
 #я подпрыгнула на стуле когда это решила
 
