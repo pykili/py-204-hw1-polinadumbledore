3d3944ae5517f18c0355648930cbbe1c8677f37e
polinadumbledore <72070019+polinadumbledore@users.noreply.github.com>
2020-10-29 18:58:40 +0300

    обновила решение

--- task_8/solution_8.py
+++ task_8/solution_8.py
@@ -2,14 +2,11 @@ summ = 0
 n = int(input())
 b = int(input())
 amount = 0
-if b == 0:
-    print(0) 
-else:
-    while b != 0:
-        summ += b
-        amount += 1
-        b = int(input())
-    print(summ/amount)
+while b != 0:
+    summ += b
+    amount += 1
+    b = int(input())
+print(summ/(amount +1))
 
 #я подпрыгнула на стуле когда это решила
 
